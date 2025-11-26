f00ec02bb79336130a5a1dae134baa07001bd23e
Tshepang Mbambo <hopsi@tuta.io>
2025-11-27 00:49:56 +0200

    succint

--- src/main.rs
+++ src/main.rs
@@ -12,11 +12,9 @@ fn main() {
         // 1.0 was released on a Friday, but nowadays they happen Thursdays
         let release = initial + WEEKS.weeks() * n - 1.days();
         if release >= Date::from(Zoned::now()) {
-            let previous_release = release - WEEKS.week();
-            println!("  {} - Rust 1.{}", previous_release, n - 1);
+            println!("  {} - Rust 1.{}", release - WEEKS.week(), n - 1);
             println!("* {} - Rust 1.{}", release, n);
-            let after_next = release + WEEKS.week();
-            println!("  {} - Rust 1.{}", after_next, n + 1);
+            println!("  {} - Rust 1.{}", release + WEEKS.week(), n + 1);
             break;
         }
     }
